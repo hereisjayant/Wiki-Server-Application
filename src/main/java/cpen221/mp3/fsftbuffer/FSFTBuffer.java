b9a57b6b788c83ff4e70c6561ec228b92dec0d9f
Vijeeth <vijeethvp@gmail.com>
2020-12-04 00:02:36 +0530

    Task 1 done except for Thread Safe Conditions

--- src/main/java/cpen221/mp3/fsftbuffer/FSFTBuffer.java
+++ src/main/java/cpen221/mp3/fsftbuffer/FSFTBuffer.java
@@ -1,5 +1,7 @@
 package cpen221.mp3.fsftbuffer;
 
+import java.util.*;
+
 public class FSFTBuffer<T extends Bufferable> {
 
     /* the default buffer size is 32 objects */
@@ -9,6 +11,83 @@ public class FSFTBuffer<T extends Bufferable> {
     public static final int DTIMEOUT = 3600;
 
     /* TODO: Implement this datatype */
+
+    private List<T> bufferList;
+
+    private Map<String, Long> timeMap;
+
+    private int capacity, timeout;
+
+    /*
+        Abstraction Function:
+            bufferList represents the finite-space finite-time buffer such that bufferList[i] is less
+                recently used than bufferList[i+1].
+
+            timeMap contains the mapping between the id of each object in the bufferList and
+                the time (in milli-seconds) when that object was added to the bufferList.
+
+            capacity represents the total number of objects that the buffer can hold
+
+            timeout represents the time (in seconds) that any unused object will be retained for
+
+        Representation Invariant:
+            bufferList should be non-null and each object in bufferList should be non-null
+
+            for every object O in bufferList, timeMap must contain key of O.id()
+
+            the size of bufferList must equal the size of timeMap, and both the sizes must be
+                less than or equal to capacity
+
+            for every (id, time of Addition) pair in timeMap, current time < time of Addition + timeout * 1000
+
+            for every legal value of i, timeMap maps bufferList[i].id() to a time that is less than or
+                before the time that timeMap maps bufferList[i+1].id() to. This is because bufferList[i]
+                is less recently used than bufferList[i+1].
+
+            capacity and timeout must both be positive integers
+     */
+
+
+    /**
+     * Check the rep invariant
+     *
+     * effects: nothing if this satisfies the rep invariant otherwise throws an exception
+     */
+    private void checkRep() {
+
+        if (!debug)
+            return;
+
+        assert bufferList != null;
+
+        for (T entry : bufferList) {
+            assert entry != null;
+            assert timeMap.containsKey(entry.id());
+        }
+
+        assert bufferList.size() == timeMap.size();
+
+        assert bufferList.size() <= capacity;
+
+        long currentTime = System.currentTimeMillis();
+        for (Long time : timeMap.values()) {
+            assert  currentTime < time + timeout * 1000;
+        }
+
+        for (int i = 0; i < bufferList.size() - 1; i++) {
+            T thisEntry = bufferList.get(i);
+            T nextEntry = bufferList.get(i+1);
+            Long thisEntryTime = timeMap.get(thisEntry.id());
+            Long nextEntryTime = timeMap.get(nextEntry.id());
+            assert thisEntryTime <= nextEntryTime;
+        }
+
+        assert capacity > 0 && timeout > 0;
+
+    }
+
+    /* Variable to control checkRep invoking */
+    static boolean debug = true;
 
     /**
      * Create a buffer with a fixed capacity and a timeout value.
@@ -21,6 +100,12 @@ public class FSFTBuffer<T extends Bufferable> {
      */
     public FSFTBuffer(int capacity, int timeout) {
         // TODO: implement this constructor
+        this.capacity = capacity;
+        this.timeout = timeout;
+        bufferList = new LinkedList<>();
+        timeMap = new HashMap<>();
+
+        checkRep();
     }
 
     /**
@@ -37,7 +122,25 @@ public class FSFTBuffer<T extends Bufferable> {
      */
     public boolean put(T t) {
         // TODO: implement this method
-        return false;
+        evictEntries();
+        checkRep();
+
+        if(timeMap.containsKey(t.id())) {
+            return false;
+        }
+
+        if (bufferList.size() == capacity) {
+            T removalEntry = bufferList.get(0);
+            bufferList.remove(removalEntry);
+            timeMap.remove(removalEntry.id());
+        }
+
+        timeMap.put(t.id(), System.currentTimeMillis());
+        bufferList.add(t);
+
+        evictEntries();
+        checkRep();
+        return true;
     }
 
     /**
@@ -45,13 +148,50 @@ public class FSFTBuffer<T extends Bufferable> {
      * @return the object that matches the identifier from the
      * buffer
      */
-    public T get(String id) {
+    public T get(String id) throws IllegalAccessException{
         /* TODO: change this */
         /* Do not return null. Throw a suitable checked exception when an object
             is not in the cache. You can add the checked exception to the method
             signature. */
-        return null;
+        evictEntries();
+        checkRep();
+
+        if (!touch(id)) {
+            throw new IllegalAccessException("The entry you want to access is not in the cache");
+        }
+
+        evictEntries();
+        checkRep();
+        return getEntryFromID(id);
     }
+
+    /**
+     * Requires that the Bufferable object with identifier id is located in the bufferList
+     *
+     * @param id the indentifier of the Bufferable object
+     * @return the corresponding Bufferable object with id that is placed in bufferList
+     */
+    private T getEntryFromID(String id) {
+        int index = getListIndexFromID(id);
+        return bufferList.get(index);
+    }
+
+    /**
+     * Requires that the Bufferable object with identifier id is located in the bufferList
+     *
+     * @param id the indentifier of the Bufferable object
+     * @return the index at which the Bufferable object is placed in bufferList
+     */
+    private int getListIndexFromID(String id) {
+        int index = 0;
+        for (T entry : bufferList) {
+            if (entry.id().equals(id)) {
+                index = bufferList.indexOf(entry);
+            }
+        }
+        return index;
+    }
+
 
     /**
      * Update the last refresh time for the object with the provided id.
@@ -63,7 +203,21 @@ public class FSFTBuffer<T extends Bufferable> {
      */
     public boolean touch(String id) {
         /* TODO: Implement this method */
-        return false;
+        evictEntries();
+        checkRep();
+
+        if (!timeMap.containsKey(id)) {
+            return false;
+        }
+        timeMap.put(id, System.currentTimeMillis());
+
+        T entryToRetrieve = getEntryFromID(id);
+        bufferList.remove(entryToRetrieve);
+        bufferList.add(entryToRetrieve);
+
+        evictEntries();
+        checkRep();
+        return true;
     }
 
     /**
@@ -76,6 +230,44 @@ public class FSFTBuffer<T extends Bufferable> {
      */
     public boolean update(T t) {
         /* TODO: implement this method */
-        return false;
+        evictEntries();
+        checkRep();
+
+        if (!touch(t.id())) {
+            return false;
+        }
+
+        int index = getListIndexFromID(t.id());
+        bufferList.set(index, t);
+
+        evictEntries();
+        checkRep();
+        return true;
+    }
+
+    /**
+     * Evicts objects that have passed the timeout from the bufferList
+     */
+    private void evictEntries() {
+        Long currentTime = System.currentTimeMillis();
+        List<String> ids = new LinkedList<>();
+        for (String id : timeMap.keySet()) {
+            Long entryTime = timeMap.get(id);
+            if (currentTime - entryTime>= timeout * 1000) {
+                ids.add(id);
+            }
+        }
+
+        for (String id : ids) {
+            timeMap.remove(id);
+            bufferList.removeIf(entry -> entry.id().equals(id));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "FSFTBuffer{" +
+                "bufferList: " + bufferList +
+                '}';
     }
 }
--- src/test/java/cpen221/mp3/Tests.java
+++ src/test/java/cpen221/mp3/Tests.java
@@ -1,11 +1,289 @@
 package cpen221.mp3;
 
+import cpen221.mp3.fsftbuffer.*;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import static org.junit.Assert.*;
+
 public class Tests {
+    private final Entry one = new Entry(1);
+    private final Entry two = new Entry(2);
+    private final Entry three = new Entry(3);
+    private final Entry four = new Entry(4);
+    private final Entry five = new Entry(5);
 
     /*
         You can add your tests here.
         Remember to import the packages that you need, such
         as cpen221.mp3.fsftbuffer.
      */
+
+    /* Task 1 Tests */
+
+    public static class Entry implements Bufferable {
+        int content;
+        String id;
+
+        public Entry (int content) {
+            this (content, Integer.toString(content));
+        }
+
+        public Entry (int content, String id) {
+            this.content = content;
+            this.id = id;
+        }
+
+        @Override
+        public String id() {
+            return id;
+        }
+
+        @Override
+        public String toString() {
+            return "Entry{" +
+                    "content: " + content +
+                    ", id: '" + id + '\'' +
+                    '}';
+        }
+    }
+
+    private boolean isNotInBuffer(FSFTBuffer<Entry> buffer, Entry entry) {
+        String expectedErrorMessage = "The entry you want to access is not in the cache";
+        boolean errorThrown = false;
+        String errorMessage = null;
+
+        try {
+            buffer.get(entry.id());
+        } catch (IllegalAccessException e) {
+            errorMessage = e.getMessage();
+            errorThrown = true;
+        }
+
+        return errorThrown && errorMessage.equals(expectedErrorMessage);
+    }
+
+    private boolean isInBuffer(FSFTBuffer<Entry> buffer, Entry entry) {
+        boolean errorThrown = false;
+        Entry actualEntry = null;
+        try {
+            actualEntry = buffer.get(entry.id());
+        } catch (IllegalAccessException e) {
+            errorThrown = true;
+        }
+
+        return !errorThrown && entry == actualEntry;
+    }
+
+    private boolean testInAndNotInBuffer(FSFTBuffer<Entry> buffer, List<Entry> inList, List<Entry> notInList) {
+        for (Entry entry : inList) {
+            if (!isInBuffer(buffer, entry)) {
+                return false;
+            }
+        }
+        for (Entry entry : notInList) {
+            if (!isNotInBuffer(buffer, entry)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void sleep(int time) {
+        try {
+            Thread.sleep(time * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sleep(double doubleTime) {
+        int time = (int) (doubleTime * 1000);
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testEvictionAfterTimeout() {
+        int timeout = 3;
+        FSFTBuffer<Entry> buffer = new FSFTBuffer<>(3, timeout);
+        buffer.put(one);
+        buffer.put(two);
+        buffer.put(three);
+
+        sleep(timeout);
+
+        assertTrue(isNotInBuffer(buffer, one));
+        assertTrue(isNotInBuffer(buffer, two));
+        assertTrue(isNotInBuffer(buffer, three));
+    }
+
+    @Test
+    public void testNonEvictionBeforeTimeout() {
+        int timeout = 3;
+        FSFTBuffer<Entry> buffer = new FSFTBuffer<>(3, timeout);
+        buffer.put(one);
+        buffer.put(two);
+        buffer.put(three);
+
+        sleep(timeout/2);
+
+        assertTrue(isInBuffer(buffer, one));
+        assertTrue(isInBuffer(buffer, two));
+        assertTrue(isInBuffer(buffer, three));
+    }
+
+    @Test
+    public void testForcedEviction1() {
+        int timeout = 3;
+        FSFTBuffer<Entry> buffer = new FSFTBuffer<>(3, timeout);
+        buffer.put(one);
+        sleep(timeout/2);
+        buffer.put(two);
+        buffer.put(three);
+
+        assertTrue( testInAndNotInBuffer(buffer, Arrays.asList(one, two, three), Collections.singletonList(four)) );
+
+        buffer.put(four);
+
+        assertTrue( testInAndNotInBuffer(buffer, Arrays.asList(two, three, four), Collections.singletonList(one)) );
+    }
+
+    @Test
+    public void testForcedEviction2() {
+        int timeout = 2;
+        FSFTBuffer<Entry> buffer = new FSFTBuffer<>(3, timeout);
+
+        buffer.put(one);
+        sleep(0.2);
+
+        buffer.put(two);
+        buffer.put(three);
+        sleep(0.3);
+
+        buffer.touch(two.id());
+        buffer.touch(one.id());
+        sleep(0.2);
+
+        buffer.put(four);
+        buffer.put(five);
+
+        assertTrue( testInAndNotInBuffer(buffer, Arrays.asList(one, four, five), Arrays.asList(two, three)) );
+
+    }
+
+    @Test
+    public void testForcedEviction3() {
+        FSFTBuffer<Entry> buffer = new FSFTBuffer<>(3, 1);
+
+        buffer.put(one);
+        buffer.put(two);
+        buffer.put(three);
+
+        sleep(0.3);
+        boolean touch1 = buffer.touch(one.id());
+        Entry updatedTwo = new Entry(100, "2");
+        boolean updated1 = buffer.update(updatedTwo);
+
+        sleep(0.5);
+        buffer.put(four);
+        boolean touch2 = buffer.touch(updatedTwo.id());
+
+        sleep(0.5);
+        assertTrue(touch1);
+        assertTrue(updated1);
+        assertTrue(touch2);
+        assertTrue( testInAndNotInBuffer(buffer, Arrays.asList(updatedTwo, four), Arrays.asList(one, three)) );
+    }
+
+    @Test
+    public void testPutFail() {
+        FSFTBuffer<Entry> buffer = new FSFTBuffer<>(3, 2);
+        boolean put1 = buffer.put(one);
+        boolean put2 = buffer.put(one);
+
+        assertTrue(put1);
+        assertFalse(put2);
+    }
+
+    @Test
+    public void testTouch1() {
+        int timeout = 2;
+        FSFTBuffer<Entry> buffer = new FSFTBuffer<>(3, timeout);
+
+        buffer.put(one);
+
+        sleep(1.5);
+        boolean updated = buffer.touch(one.id());
+
+        sleep(1.5);
+
+        assertTrue(isInBuffer(buffer,one));
+        assertTrue(updated);
+    }
+
+    @Test
+    public void testTouch2() {
+        int timeout = 2;
+        FSFTBuffer<Entry> buffer = new FSFTBuffer<>(3, timeout);
+
+        buffer.put(one);
+
+        sleep(1.5);
+        boolean updated = buffer.touch(one.id());
+
+        sleep(2.0);
+
+        assertTrue(isNotInBuffer(buffer,one));
+        assertTrue(updated);
+
+    }
+
+    @Test
+    public void testTouchFail() {
+        FSFTBuffer<Entry> buffer = new FSFTBuffer<>(3, 2);
+
+        boolean updated = buffer.touch(one.id());
+
+        sleep(2.0);
+
+        assertTrue(isNotInBuffer(buffer,one));
+        assertFalse(updated);
+
+    }
+
+    @Test
+    public void testUpdate1() {
+        int timeout = 2;
+        FSFTBuffer<Entry> buffer = new FSFTBuffer<>(3, timeout);
+
+        buffer.put(one);
+        sleep(1.0);
+
+        Entry updatedOne = new Entry(12, "1");
+        boolean updated = buffer.update(updatedOne);
+
+        assertTrue( isInBuffer(buffer, updatedOne) );
+        assertTrue(updated);
+    }
+
+    @Test
+    public void testUpdateFail() {
+        FSFTBuffer<Entry> buffer = new FSFTBuffer<>(3, 2);
+
+        buffer.put(one);
+
+        sleep(3);
+        Entry updatedOne = new Entry(13, "1");
+        boolean updated = buffer.update(updatedOne);
+
+        assertTrue( isNotInBuffer(buffer, updatedOne) );
+        assertFalse(updated);
+    }
 
 }
