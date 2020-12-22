64b53948d46f645da6243a9af7b3d8cf7e110296
hereisjayant <hereisjayant@gmail.com>
2020-12-23 00:27:33 +0530

    Added executor service to the server to manage upto n requests concurrently

--- src/main/java/cpen221/mp3/server/WikiMediatorServer.java
+++ src/main/java/cpen221/mp3/server/WikiMediatorServer.java
@@ -1,6 +1,17 @@
 package cpen221.mp3.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class WikiMediatorServer {
+
+
+    private final ServerSocket serverSocket;
+    private final ExecutorService pool;
+
 
     /**
      * Start a server at a given port number, with the ability to process
@@ -9,8 +20,53 @@ public class WikiMediatorServer {
      * @param port the port number to bind the server to
      * @param n the number of concurrent requests the server can handle
      */
-    public WikiMediatorServer(int port, int n) {
-        /* TODO: Implement this method */
+    public WikiMediatorServer(int port, int n) throws IOException {
+        serverSocket = new ServerSocket(port);
+        pool = Executors.newFixedThreadPool(n);
+    }
+
+    /**
+     * Run the server, listening for connections and handling them.
+     *
+     * @throws IOException
+     *             if the main server socket is broken
+     */
+    public void serve() throws IOException {
+        while (true) {
+            // block until a client connects
+            final Socket socket = serverSocket.accept();
+
+            //Creates a new executer service to handle n requests:
+            pool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        try {
+                            handle(socket);
+                        } finally {
+                            socket.close();
+                        }
+                    } catch (IOException ioe) {
+                        // this exception wouldn't terminate serve(),
+                        // since we're now on a different thread, but
+                        // we still need to handle it
+                        ioe.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+    /**
+     * Handle one client connection. Returns when client disconnects.
+     *
+     * @param socket
+     *            socket where client is connected
+     * @throws IOException
+     *             if connection encounters an error
+     */
+    private void handle(Socket socket) throws IOException {
+
     }
 
 }
